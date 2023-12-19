cd5ea6172edf4e4a7d0d8e4564034399ca1aaf33
bikashroy120 <bikashroydt@gmail.com>
2023-12-19 21:34:58 +0600

    nearby job card and section add

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './nearbyjobcard.style'
 
-const NearbyJobCard = () => {
+const NearbyJobCard = ({item,handleNavigate}) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
+      <TouchableOpacity
+        style={styles.container}
+        onPress={()=>handleNavigate()}
+      >
+        <TouchableOpacity style={styles.logoContainer}>
+            <Image 
+              source={{uri:item.employer_logo}}
+              style={styles.logoImage}
+            />
+        </TouchableOpacity>
+        <View style={styles.textContainer}>
+            <Text style={styles.jobName} numberOfLines={1} >{item.job_title}</Text>
+            <Text style={styles.jobType}>{item.job_employment_type}</Text>
+        </View>
+      </TouchableOpacity>
   )
 }
 
